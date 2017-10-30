693aa50fb2b3688eb5e1849f1609027ee00be75e
PatricioMaturo <32202550+PatricioMaturo@users.noreply.github.com>
2017-10-30 18:23:36 -0300

    Create Viaje.php
    Creamos la clase Viaje

---
+++ src/Viaje.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TpFinal;
+
+class Viaje{
+
+
+    protected $tipo;
+    protected $fecha;
+    protected $monto;
+    protected $transporte;
+
+    public function __construct ($tipo,$monto,$transporte){
+    
+    $this->tipo=$tipo
+    $this->monto=$monto
+    $this->transporte=$transporte
+    
+    
+    }
+
+}
